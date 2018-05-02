01870c19b85284ff7fc61f14b9d0dcd38acb2de8
Diego Andres <azlinraeh@gmail.com>
2018-05-02 10:15:14 +0200

    Nav Finish

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("#hamburger1").click(function(){
+    $("#hamburger1").attr("class").includes("is-active")?
+      $('.target').show("swing"):
+      $('.target').hide("swing");
+   });
+});
--- js/index2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#hamburger1").click(function(){
-      $("#hamburger1").attr("class").includes("is-active")?
-        $('.target').show("swing"):
-        $('.target').hide("swing");
-     });
-  });
